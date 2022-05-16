1c53f281a020d3d82800fb11cb8775965e68abbd
Adam Januszewski <Adpjan@att.net>
2022-05-16 12:31:55 -0400

    Initial Commit

---
+++ Africa/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Africa
+//
+//  Created by adam janusewski on 5/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
